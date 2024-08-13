03c27a5821feeb96affc211b8e0582558675b129
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-08-13 17:47:55 -0300

    Correção

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { reactive } from "vue";
+  import { reactive, watch } from "vue";
 
   const estado = reactive({
     ValorA: 0,
@@ -25,7 +25,9 @@
     }
   }
 
-
+  watch([() => estado.ValorA, () => estado.ValorB, () => estado.operacao], () => {
+    AlteraOperacao();
+  });
 
 
 
@@ -34,18 +36,20 @@
 <template>
   <div class="container mt-5 ">
     <div class="row">
-      <form  @keyup="AlteraOperacao()" class="form-control">
+      <form @change="AlteraOperacao()" class="form-control">
         <select @change="evento => estado.operacao = evento.target.value" class="col ms-5 mb-2 btn btn-primary ">
-        <option value="somar">somar</option>
-        <option value="subtrair">subtrair</option>
-        <option value="multiplicar">multiplicar</option>
-        <option value="Dividir">Dividir</option>
-      </select>
-      <ul class="list-grup row">
-        <input class="form-control col" type="number" :value="estado.ValorA" @keyup="evento => estado.ValorA =  parseInt(evento.target.value)" place-holder="escolha o Primeiro valor">
-        <input class="form-control col" type="number" :value="estado.ValorB" @keyup="evento => estado.ValorB =  parseInt(evento.target.value)" place-holder="escolha o Segundo valor">
-      </ul>
-        <span class="col btn btn-success" >O resutado é {{ estado.VAlorResultado }}</span>
+          <option value="somar">somar</option>
+          <option value="subtrair">subtrair</option>
+          <option value="multiplicar">multiplicar</option>
+          <option value="Dividir">Dividir</option>
+        </select>
+        <ul class="list-grup row">
+          <input class="form-control col" type="number" :value="estado.ValorA"
+            @keyup="evento => estado.ValorA = parseInt(evento.target.value)" place-holder="escolha o Primeiro valor">
+          <input class="form-control col" type="number" :value="estado.ValorB"
+            @keyup="evento => estado.ValorB = parseInt(evento.target.value)" place-holder="escolha o Segundo valor">
+        </ul>
+        <span class="col btn btn-success">O resultado é {{ estado.VAlorResultado }}</span>
       </form>
     </div>
   </div>
